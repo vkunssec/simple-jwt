29332254d93d52d9fe83a5e4d8e59c8108d65604
Victor <vkunssec@gmail.com>
2024-02-29 17:08:31 -0300

    fix: lint

--- src/core/utils/logger.ts
+++ src/core/utils/logger.ts
@@ -2,13 +2,13 @@ import pino from "pino";
 import dayjs from "dayjs";
 
 const logger = pino({
-  transport: {
-    target: "pino-pretty",
-    options: {
-      translateTime: dayjs().format(),
-      ignore: "pid,hostname",
-    },
-  },
+	transport: {
+		target: "pino-pretty",
+		options: {
+			translateTime: dayjs().format(),
+			ignore: "pid,hostname",
+		},
+	},
 });
 
 export default logger;
